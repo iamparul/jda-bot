9271758a206e6e7940703265e22aafedc9b437b5
parul thakur <punitthakur1010@gmail.com>
2021-06-04 00:46:43 +0530

    main java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,34 @@
-public class Main {
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+import javax.security.auth.login.LoginException;
+
+public class Main extends ListenerAdapter {
+
+    public static JDABuilder builder;
+
+    public static void main(String[] args) throws LoginException, InterruptedException {
+        builder = new JDABuilder(AccountType.BOT);
+        String Token = "ODQ5NzIyMTQ1NzE3NzQ3Nzgy.YLfTag.Gb91pLtIUIEHLwbv1ggwo5Cvf1k";
+        builder.setToken(Token);
+        builder.addEventListener(new Main());
+//        builder.buildAsync();
+    }
+
+    public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
+        System.out.println("Message Received from" +
+                event.getAuthor().getName() + ": " + event.getMessage().getContentDisplay());
+//        System.out.println("abc"+event.getAuthor());
+        if (event.getAuthor().isBot())
+            return;
+        if (event.getMessage().getContentRaw().equalsIgnoreCase("hi")) {
+            event.getChannel().sendMessage("Hello").queue();
+        } else {
+            event.getChannel().sendMessage("else case").queue();
+        }
+
+//        GuildMessageReceivedEvent
+    }
 }
